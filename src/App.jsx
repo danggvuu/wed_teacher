7050d5ff8948a297d61e3915aa44a9af15fcb65d
dangvu <dangvu@MacBook-Pro-cua-dangvu.local>
2026-07-02 14:08:06 +0700

    fix: redirect unknown routes to home

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import TeacherDetail from './pages/TeacherDetail';
 import AdminLogin from './pages/AdminLogin';
@@ -12,6 +12,8 @@ function App() {
         <Route path="/giao-vien/:slug" element={<TeacherDetail />} />
         <Route path="/admin/login" element={<AdminLogin />} />
         <Route path="/admin" element={<AdminDashboard />} />
+        {/* Chuyển hướng các trang lỗi về trang chủ */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
